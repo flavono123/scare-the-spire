a18378213ccc400220c466d3ba44d315c7c8aa62
flavono123 <flavono123@gmail.com>
2026-05-23 16:43:16 +0900

    Fix path locale hook return type

--- src/hooks/use-service-locale.ts
+++ src/hooks/use-service-locale.ts
@@ -1,15 +1,10 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import {
-  getServiceLocaleForGameLocale,
-  getServiceLocaleFromPath,
-  type ServiceLocale,
-} from "@/lib/i18n";
+import { getServiceLocaleFromPath, type ServiceLocale } from "@/lib/i18n";
 
 export function useServiceLocale(): ServiceLocale {
   const pathname = usePathname() ?? "/";
-  const pathServiceLocale = getServiceLocaleFromPath(pathname);
 
-  return getServiceLocaleForGameLocale(pathServiceLocale.gameLocale);
+  return getServiceLocaleFromPath(pathname);
 }
