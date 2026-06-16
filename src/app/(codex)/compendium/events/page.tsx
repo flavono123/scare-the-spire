54eebe6b13b6d2c5b96b8fcdd86c047b7df216dc
flavono123 <flavono123@gmail.com>
2026-06-16 23:50:35 +0900

    Keep compendium indexes static on Workers

--- src/app/(codex)/compendium/events/page.tsx
+++ src/app/(codex)/compendium/events/page.tsx
@@ -1,16 +1,16 @@
 export const dynamic = "force-static";
 
 import BasePage, { generateMetadata as generateBaseMetadata } from "../../_codex/events/page";
-import { defaultRouteSearchParams, type RouteSearchParams } from "../static-locale";
+import { defaultRouteSearchParams } from "../static-locale";
 
-export function generateMetadata({ searchParams }: { searchParams: RouteSearchParams }) {
+export function generateMetadata() {
   return generateBaseMetadata({
-    searchParams: defaultRouteSearchParams(searchParams),
+    searchParams: defaultRouteSearchParams(),
   });
 }
 
-export default function CompendiumEventsPage({ searchParams }: { searchParams: RouteSearchParams }) {
+export default function CompendiumEventsPage() {
   return BasePage({
-    searchParams: defaultRouteSearchParams(searchParams),
+    searchParams: defaultRouteSearchParams(),
   });
 }
