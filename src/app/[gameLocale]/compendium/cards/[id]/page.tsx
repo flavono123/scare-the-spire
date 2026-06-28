bc073e1290acba4c342bca0e7260465b098824ce
flavono123 <flavono123@gmail.com>
2026-06-28 15:12:19 +0900

    Static render localized card details

--- src/app/[gameLocale]/compendium/cards/[id]/page.tsx
+++ src/app/[gameLocale]/compendium/cards/[id]/page.tsx
@@ -7,29 +7,17 @@ export const dynamic = "force-static";
 export const dynamicParams = false;
 type Props = {
   params: Promise<LocaleRouteParams<{ id: string }>>;
-  searchParams: Promise<Record<string, string | string[] | undefined>>;
 };
 
 export async function generateStaticParams() {
   return generateLocalizedStaticParams(generateCardStaticParams);
 }
 
-async function localizedSearchParams(
-  gameLocale: Parameters<typeof searchRecordForGameLocale>[0],
-  searchParams: Props["searchParams"],
-) {
-  const resolvedSearchParams = await searchParams;
-  return {
-    ...resolvedSearchParams,
-    ...searchRecordForGameLocale(gameLocale),
-  };
-}
-
-export async function generateMetadata({ params, searchParams }: Props) {
+export async function generateMetadata({ params }: Props) {
   const { gameLocale, id } = await getLocalePairFromParams(params);
   return generateBaseMetadata({
     params: Promise.resolve({ id }),
-    searchParams: localizedSearchParams(gameLocale, searchParams),
+    searchParams: Promise.resolve(searchRecordForGameLocale(gameLocale)),
   });
 }
 
