66efaa2a429870ed3b993900b8573f823340d753
flavono123 <flavono123@gmail.com>
2026-07-10 04:35:22 +0900

    Load story placeholder localization from filesystem

--- src/lib/sts2-game-ui-copy.ts
+++ src/lib/sts2-game-ui-copy.ts
@@ -1,25 +1,8 @@
 import type { GameLocale } from "./i18n";
+import { readGameLocalizationTable } from "./game-localization";
 
 const STORY_COMPOSER_PLACEHOLDER_KEY = "OROBAS.talk.REGENT.1-0r.ancient";
 const STORY_COMPOSER_PLACEHOLDER_FALLBACK = "Now tell more story!!";
-type AncientLocalization = Partial<Record<string, string>>;
-
-const ancientLocalizationLoaders: Record<GameLocale, () => Promise<AncientLocalization>> = {
-  deu: () => import("../../data/sts2/localization/deu/ancients.json").then((module) => module.default),
-  eng: () => import("../../data/sts2/localization/eng/ancients.json").then((module) => module.default),
-  esp: () => import("../../data/sts2/localization/esp/ancients.json").then((module) => module.default),
-  fra: () => import("../../data/sts2/localization/fra/ancients.json").then((module) => module.default),
-  ita: () => import("../../data/sts2/localization/ita/ancients.json").then((module) => module.default),
-  jpn: () => import("../../data/sts2/localization/jpn/ancients.json").then((module) => module.default),
-  kor: () => import("../../data/sts2/localization/kor/ancients.json").then((module) => module.default),
-  pol: () => import("../../data/sts2/localization/pol/ancients.json").then((module) => module.default),
-  ptb: () => import("../../data/sts2/localization/ptb/ancients.json").then((module) => module.default),
-  rus: () => import("../../data/sts2/localization/rus/ancients.json").then((module) => module.default),
-  spa: () => import("../../data/sts2/localization/spa/ancients.json").then((module) => module.default),
-  tha: () => import("../../data/sts2/localization/tha/ancients.json").then((module) => module.default),
-  tur: () => import("../../data/sts2/localization/tur/ancients.json").then((module) => module.default),
-  zhs: () => import("../../data/sts2/localization/zhs/ancients.json").then((module) => module.default),
-};
 
 function lastNonEmptyLine(text: string) {
   return text
@@ -30,7 +13,7 @@ function lastNonEmptyLine(text: string) {
 }
 
 async function loadAncientLocalization(gameLocale: GameLocale) {
-  return ancientLocalizationLoaders[gameLocale]();
+  return readGameLocalizationTable(gameLocale, "ancients");
 }
 
 export async function getStoryComposerPlaceholder(gameLocale: GameLocale) {
