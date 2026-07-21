4eb53779bb0bb7da2066cf23a3ac18571938b1ed
flavono123 <flavono123@gmail.com>
2026-07-21 09:46:31 +0900

    Block Amazonbot crawling

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,6 +5,10 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
       {
+        userAgent: "Amazonbot",
+        disallow: "/",
+      },
+      {
         userAgent: "*",
         allow: "/",
         // Keep duplicate legacy routes and internal development pages out of search.
