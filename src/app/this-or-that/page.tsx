6906c02db5934ed831b3c49ba82e9267579d157b
flavono123 <flavono123@gmail.com>
2026-07-22 09:46:43 +0900

    Add event backdrop to This or That

--- src/app/this-or-that/page.tsx
+++ src/app/this-or-that/page.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { ThisOrThatClient } from "@/components/this-or-that/this-or-that-client";
+import { ServiceBackground } from "@/components/service-background";
 import { getThisOrThatGameCopy } from "@/lib/borrowed-game-copy";
 import { getServiceLocaleForGameLocale, type GameLocale } from "@/lib/i18n";
 import { DEFAULT_ROUTE_GAME_LOCALE } from "@/lib/locale-routing";
@@ -30,12 +31,18 @@ export async function renderThisOrThatPage(
   const gameCopy = await getThisOrThatGameCopy(gameLocale);
 
   return (
-    <div className="mx-auto max-w-6xl px-4 py-6">
-      <ThisOrThatClient
-        gameLocale={gameLocale}
-        title={gameCopy.title}
-        prompt={gameCopy.prompt}
+    <div className="relative isolate min-h-[calc(100svh-3rem)]">
+      <ServiceBackground
+        src="/images/sts2/events/this_or_that.webp"
+        imageClassName="object-[38%_center] sm:object-center"
       />
+      <div className="mx-auto max-w-6xl px-4 py-6">
+        <ThisOrThatClient
+          gameLocale={gameLocale}
+          title={gameCopy.title}
+          prompt={gameCopy.prompt}
+        />
+      </div>
     </div>
   );
 }
