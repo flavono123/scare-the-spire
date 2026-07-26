848db3cf4aa55e3eff7f7ccdb90e685c28afbd6e
flavono123 <flavono123@gmail.com>
2026-07-27 00:01:13 +0900

    Localize Transfigure image filenames

--- src/components/transfigure/transfigure-resource-preview.tsx
+++ src/components/transfigure/transfigure-resource-preview.tsx
@@ -65,7 +65,7 @@ export function TransfigureResourcePreview({
     downloadSuccess: copy.assetImageDownloaded,
     downloadFailed: copy.assetImageDownloadFailed,
   };
-  const imageFileName = `변형-${displayName}.png`;
+  const imageFileName = copy.assetImageFileName.replace("{name}", displayName);
 
   if (entity.type === "card" && entity.cardData) {
     const effectiveUpgradeBlocks = upgradedBlocks ?? gameUpgradeBlocks;
